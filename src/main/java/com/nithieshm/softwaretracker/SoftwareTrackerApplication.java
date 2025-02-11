dc7972939b7a8167b388b2ec893c261f9a072724
Nithiesh Mohan <nithiesh164@gmail.com>
2025-02-11 15:00:00 +0530

    Created Users entity.

--- src/main/java/com/nithieshm/softwaretracker/SoftwareTrackerApplication.java
+++ src/main/java/com/nithieshm/softwaretracker/SoftwareTrackerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SoftwareTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SoftwareTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareTrackerApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/nithieshm/softwaretracker/entities/Users.java
@@ -0,0 +1,49 @@
+package com.nithieshm.softwaretracker.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    public Users(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public Users() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
