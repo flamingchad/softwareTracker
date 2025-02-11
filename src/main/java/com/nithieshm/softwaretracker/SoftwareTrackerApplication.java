388de89ad5e11f40b6b49a1e895f668ba2f030fe
Nithiesh Mohan <nithiesh164@gmail.com>
2025-02-11 14:52:44 +0530

    Initial commit

---
+++ src/main/java/com/nithieshm/softwaretracker/SoftwareTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.nithieshm.softwaretracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nithieshm/softwaretracker/SoftwareTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nithieshm.softwaretracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
